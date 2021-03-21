8d3023a532f8755c2fa26f6ffe5a0d0917ac15c2
Sharon Martinez <martinezsharonr@gmail.com>
2021-03-21 01:36:37 -0800

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,6 @@
-//require mysql
 const mysql = require('mysql');
 
-//creates connection info,using credentials to mysql database 
+//creates connection
 var connection = mysql.createConnection({
   host     : 'localhost',
   port     : '3306',
@@ -11,9 +10,7 @@ var connection = mysql.createConnection({
   database: 'employees_DB'
 });
 
-// connect and catch errors
 connection.connect((err) => {
   if (err) throw err;
 });
-// exports module
 module.exports = connection;
